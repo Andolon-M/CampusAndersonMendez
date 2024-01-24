80f9e736cea1646fa79214562d1f3c3713355520
vacio <vacio>
2024-01-23 19:44:06 -0500

    tercera prueba de logout

--- Clase 23-01-2024/Python/introduccion.py
+++ Clase 23-01-2024/Python/introduccion.py
@@ -3,3 +3,10 @@ numero = 10
 palabra = "ejemplo"
 Condicion = True
 variable3 = ""
+
+print (type (numero))
+print(type (palabra))
+print(type (Condicion))
+
+Condicion = "no"
+print(type (Condicion))
